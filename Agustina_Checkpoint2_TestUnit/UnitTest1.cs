18e9043f7e9d4b71d1be470008029e66d3c81bd0
AgustinaScapusio <agustina.scapusio.99@gmail.com>
2023-11-13 09:19:32 +0100

    Added UnitTest

---
+++ Agustina_Checkpoint2_TestUnit/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Agustina_Checkpoint2_TestUnit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
